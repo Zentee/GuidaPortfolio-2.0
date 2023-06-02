1e92cf35a6aa1b0a57e1e6a943e4a6fee3bbdc9b
zentee <filipeyum@gmail.com>
2023-06-02 22:07:17 +0900

    update:test

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '@styles/globals.css'
+import "@styles/globals.css";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
+import HeadDetails from "@components/HeadDetails";
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <HeadDetails />
+      <Header />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
-export default Application
+export default Application;
