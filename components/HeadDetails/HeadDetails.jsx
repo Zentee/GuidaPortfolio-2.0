5ffe4ccf1cbf560af74bf32a6207c6184293d07d
Filipe Yum <85417874+Zentee@users.noreply.github.com>
2024-01-04 20:55:30 +0900

    update: font updates and copy

--- components/HeadDetails/HeadDetails.jsx
+++ components/HeadDetails/HeadDetails.jsx
@@ -6,9 +6,18 @@ function HeadDetails() {
       <title>Ana Cunha Portfolio</title>
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
       <link
         href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500"
+        rel="stylesheet"
+      />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
         rel="stylesheet"
       />
       <link
