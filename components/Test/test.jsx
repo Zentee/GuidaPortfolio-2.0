66cc75693b4655eb0f5c467ddb1abf7d051ffd6a
Filipe Yum <85417874+Zentee@users.noreply.github.com>
2024-05-06 19:51:16 +0100

    update: added color to background

--- components/Test/test.jsx
+++ components/Test/test.jsx
@@ -2,8 +2,8 @@ import React, { useState, useEffect } from "react";
 
 const Test = () => {
   return (
-    <div>
-      <p style={{ color: "white" }}>hello</p>
+    <div style={{ backgroundColor: "white" }}>
+      <p>hello</p>
     </div>
   );
 };
